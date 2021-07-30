87d6badc10a2715392486e1b40560196f58f9c29
Mathias Regnier <mathiasregnier@macbook-pro.home>
2021-07-30 22:28:57 +0200

    Adding english version

--- put_on_github.sh
+++ put_on_github.sh
@@ -3,12 +3,14 @@
 
 # Move file from Desktop/HTML to Desktop/HTML/mathias77515.github.io
 
-mv Users/mathiasregnier/Desktop/HTML/parcours.html Users/mathiasregnier/Desktop/HTML/mathias77515.github.io/
-mv Users/mathiasregnier/Desktop/HTML/stageM1.html Users/mathiasregnier/Desktop/HTML/mathias77515.github.io/
-mv Users/mathiasregnier/Desktop/HTML/stageM2.html Users/mathiasregnier/Desktop/HTML/mathias77515.github.io/
-mv Users/mathiasregnier/Desktop/HTML/contact.html Users/mathiasregnier/Desktop/HTML/mathias77515.github.io/
-mv Users/mathiasregnier/Desktop/HTML/index.html Users/mathiasregnier/Desktop/HTML/mathias77515.github.io/
-mv Users/mathiasregnier/Desktop/HTML/style.css Users/mathiasregnier/Desktop/HTML/mathias77515.github.io/
+cp picture/* mathias77515.github.io/picture
+cp parcours* mathias77515.github.io
+cp stageM1* mathias77515.github.io
+cp stageM2* mathias77515.github.io
+cp skills* mathias77515.github.io
+cp contact* mathias77515.github.io
+cp index* mathias77515.github.io
+cp style.css mathias77515.github.io
 
 cd mathias77515.github.io
 
@@ -18,7 +20,7 @@ cd mathias77515.github.io
 git add --all
 
 # Commit the new changements
-git commit -m "New version with sh file"
+git commit -m "Adding english version"
 
 # Pushing
 git push -u origin master
