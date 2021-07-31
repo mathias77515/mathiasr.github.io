39e6eb93a6aa5318e0ad31763342ba232868b731
Mathias Regnier <mathiasregnier@macbook-pro.home>
2021-07-31 11:47:19 +0200

    New version 30/07/2021

---
+++ put_on_github.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+# Move file from Desktop/HTML to Desktop/HTML/mathias77515.github.io
+
+cp picture/* mathias77515.github.io/picture
+cp parcours* mathias77515.github.io
+cp stageM1* mathias77515.github.io
+cp stageM2* mathias77515.github.io
+cp skills* mathias77515.github.io
+cp contact* mathias77515.github.io
+cp index* mathias77515.github.io
+cp style.css mathias77515.github.io
+
+cd mathias77515.github.io
+
+
+
+# add all files in GitHub repository
+git add --all
+
+# Commit the new changements
+git commit -m "New version 30/07/2021"
+
+# Pushing
+git push -u origin master
